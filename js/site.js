8fa8db0194ea3d2d7a385eaa0abe91410fd7fdbb
megamind <janakiraman@proisc.com>
2017-03-27 00:30:43 +0530

    Updated the following files for the form validation using angular js.

--- js/site.js
+++ js/site.js
@@ -1,2 +1,28 @@
-$('.jumbotron span').datepicker({
+var validationApp = angular.module('validationApp', []);
+// create angular controller
+validationApp.controller('mainController', function($scope) {
+
+  // function to submit the form after all validation has occurred            
+  $scope.submitForm = function(isValid) {
+  	$scope.submitted = true;
+    // check to make sure the form is completely valid
+    if (isValid) {
+      alert('Form is a valid data!');
+    }
+
+  };
+  // Added the dp.change has the field is 
+  // not automatically recognized on changing the value
+	$scope.start_time = '';
+	$('#start_time_picker').on('dp.change', function(e){ 
+		$scope.start_time = $('#start_time').val(); 
+	});
 });
+$(function () {
+	$('#start_date_picker').datepicker({format: 'M dd, yyyy'});
+	$('#town_start_date_picker').datepicker({format: 'M dd, yyyy'});
+	$('#town_end_date_picker').datepicker({format: 'M dd, yyyy'});
+	$('#start_time_picker').datetimepicker({format: 'LT'});
+});
+
+
